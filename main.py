b803feb71fb72b5db2b3fb6d25a76bdf1f0e5322
Colton Leatherman <coltonjleatherman@gmail.com>
2024-02-12 18:25:14 -0500

    Added in webapp (not currently working)

--- main.py
+++ main.py
@@ -1,23 +1,128 @@
-# Internal Imports
-from classes import allocatorUtil
-from classes import event
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+from string import ascii_uppercase
+import random
+import json
 
-# External Imports
-import argparse
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "hjhjsdahhds"
+app.config['host'] = '0.0.0.0'
+socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+rooms = {}
 
-    parser.add_argument("--file", "-f", type=str, required=True, help="Name of input file")
-    args = parser.parse_args()
+@app.route('/input-preferences', methods=["POST", "GET"])
+def input_preferences():
+    preferences = {}
+    room = session.get('room')
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
 
-    if args.file is not None:
-        print(f"Loading in file: {args.file}")
-        # First, an event needs to be set. This contains all the players, their preference scores and the game options
-        event = event.Event()
-        event.from_text_file(args.file)
-    else:
-        print("No file provided")
+    if request.method == "POST":
+        for game in session['games']:
+            preferences[game] = request.form.get(f"{game}-inlineRadioOptions")
 
-    solver = allocatorUtil.Solver(event)
-    solver.check_all_combinations()
+        session['preferenceScores'] = preferences
+        return redirect(url_for('room'))
+
+    return render_template("input_preferences.html", games=session['games'], roomcode=session['room'], name=session['name'])
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    session.clear()
+    games = ["Dune", "Star Realms", "PanAm", "Catan", "MTG"]
+    games.sort()
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        selectedGames = request.form.getlist("selectedGames")
+
+        if join != False and (not code or len(code) != 4):
+            return render_template("home.html", error="Please enter a valid room code.", code=code, name=name)
+        room = code
+        # Determine if POST request is from join or create
+        if create != False: # User pressed "create" button and selected games.
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "games": selectedGames, 'preferences':[]}
+        elif code not in rooms: # User attempts to join a room
+            return render_template("home.html", error="Room does not exist", len = len(games), games = games, code=code, name=name)
+
+        session['room'] = room
+        session['name'] = name
+        session['games'] = rooms[room]['games']
+        print(rooms[room]['games'])
+
+        return redirect(url_for('input_preferences'))
+
+    return render_template("home.html", games = games)
+
+@app.route('/room')
+def room():
+    room = session.get('room')
+    if room is None or session.get('name') is None or room not in rooms:
+        return redirect(url_for('home'))
+    return render_template('room.html', name=session['name'], room=session['room'], games=session['games'], preferenceScores = session['preferenceScores'])
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get('room')
+    name = session.get('name')
+
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    data = {"name": name, "scores": session.get('preferenceScores')}
+    print(data)
+    send(data, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    # If the last person has leaves, delete the room
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    #send({"name": name, "scores": "has left the room"}, to=room)
+    print(f"{name} has theft the room {room}")
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    
+    content = {
+        "name": session.get("name"),
+        "scores": session.get("preferenceScores")
+    }
+    #send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+    
+    return code
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', debug=True)
